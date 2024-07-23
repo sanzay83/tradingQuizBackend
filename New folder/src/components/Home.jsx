2b3f4374b1316f27e329765b9e34d8373e8f7367
Sanjay Duwal <sanzay8342258@gmail.com>
2024-07-23 00:30:06 -0500

    removing files that are required

--- New folder/src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <div>This is empty Home page.</div>;
-}
-
-export default Home;
--- New folder/src/components/Stats.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Stats() {
-  return <div>This is a stats page.</div>;
-}
-
-export default Stats;
