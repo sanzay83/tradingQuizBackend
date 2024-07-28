ad329015486420e950e8411afffd15ac8e303f36
Sanjay Duwal <sanzay8342258@gmail.com>
2024-07-27 20:23:16 -0500

    added study api

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ promisePool
 
 // Define routes
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello Developer, The backend server is running!");
 });
 
 app.post("/items", async (req, res) => {
@@ -80,6 +80,62 @@ app.delete("/items/:id", async (req, res) => {
       return res.status(404).json({ message: "item not found" });
     }
     res.json({ message: "Item deleted" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to delete item" });
+  }
+});
+
+app.get("/study/items/:type", async (req, res) => {
+  const { type } = req.params;
+  console.log(type);
+  try {
+    const [rows] = await promisePool.execute(
+      "SELECT * FROM `StudyMaterial` WHERE `type`=?",
+      [type]
+    );
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to retrieve items" });
+  }
+});
+
+app.get("/study/items", async (req, res) => {
+  try {
+    const [rows] = await promisePool.execute("SELECT * FROM StudyMaterial");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to retrieve items" });
+  }
+});
+
+app.post("/study/items", async (req, res) => {
+  const { title, thumbnail, exampleimage, description, type, id } = req.body;
+  try {
+    await promisePool.execute(
+      "INSERT INTO StudyMaterial (title, thumbnail, exampleimage, description, type, id) VALUES (?, ?, ?, ?, ?, ?)",
+      [title, thumbnail, exampleimage, description, type, id]
+    );
+    res.json({ message: "Item added" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to add item" });
+  }
+});
+
+app.delete("/study/items/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [result] = await promisePool.execute(
+      "DELETE FROM StudyMaterial WHERE id = ?",
+      [id]
+    );
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "item not found" });
+    }
+    res.json({ message: "Study Material deleted" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Failed to delete item" });
